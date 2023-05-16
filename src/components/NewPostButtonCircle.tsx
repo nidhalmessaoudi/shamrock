c5bc9d9eaa63d2ae6dc1ae6180640b12e5761506
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2023-05-16 01:17:31 +0100

    Changing icon color

--- src/components/NewPostButtonCircle.tsx
+++ src/components/NewPostButtonCircle.tsx
@@ -5,7 +5,7 @@ export default function NewPostButtonCircle(props: { handler: () => void }) {
       onClick={props.handler}
       className="fixed bottom-4 right-8 block rounded-full bg-green-blue px-4 py-3 dark:bg-light-green sm:right-32 min-[1228px]:hidden"
     >
-      <i className="bi bi-pencil-square text-xl leading-[inherit]"></i>
+      <i className="bi bi-pencil-square text-xl leading-[inherit] text-white"></i>
     </div>
   );
 }
